dc4058bf8fb8f0db195946f6376d4cc5cdc2b567
David Maluenda <dmaluenda@ub.edu>
2025-07-11 17:21:49 +0200

    Making up

--- src/unify_CV_columns.py
+++ src/unify_CV_columns.py
@@ -1,18 +1,76 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import sys
+import sys, os
+import glob
 import numpy as np
 import pandas as pd
 import regex
 
 from statistics import mean
 
+UNIFIED_SUFFIX = "_unified"
+GRUP_LAB = "gLab."
+GRUP_TEO = "gTeo."
+
+ORAL_NEW = "Oral"
+ORAL_PATTERN = "prova oral"
+ORAL_EXPECTED = 1
+
+INFORMES_NEW = "Informes"
+INFORMES_PATTERN = "entrega informes"
+INFORMES_EXPECTED = 7
+
+MISSING_SESSIONS = "Missing sessions"
+
+POSSIBLE_GRUPS_LAB = "M1A|M1B|M2A|M2B|T1A|T1B"
+GRUPS_TEO = ['Q2_M1', 'Q2_M2', 'Q2_T1', 'Q1_T1']
+Q1T1_LABEL = 'ReAv.'
+SESSION_PATTERN = r"Sessió\s+(\d+)\s+"
+GRUPS_LABEL_CV = 'Grups'
+NIUB = 'Número ID'
+
+FINAL_KEYS = ['Nom', 'Cognoms', NIUB, 
+              ORAL_NEW, INFORMES_NEW, MISSING_SESSIONS, 
+              GRUP_TEO, GRUP_LAB]
+
+def load_main(pattern):
+    fn_not_exists = True
+
+    dir_list = [fn for fn in glob.glob(pattern) if UNIFIED_SUFFIX not in fn]
+    excel_filename = dir_list[0] if len(dir_list) == 1 else None
+    fn_not_exists = not os.path.isfile(excel_filename or '')
+
+    while fn_not_exists:
+        excel_filename = input("Nom del fitxer Excel:")
+        excel_filename = (excel_filename if excel_filename.endswith('.xlsx') 
+                          else excel_filename+'.xlsx')
+        fn_not_exists = not os.path.isfile(excel_filename)
+        print(f"\n >>> {excel_filename} does NOT found. Try again," if fn_not_exists 
+              else "Found!")
+
+    return pd.read_excel(excel_filename, sheet_name=0), excel_filename
+
+def load_part(pattern):
+
+    dir_list2 = [fn for fn in glob.glob(pattern)]
+    excel_fn_part = dir_list2[0] if len(dir_list2) == 1 else None
+    part_exists = os.path.isfile(excel_fn_part or '')
+
+    while not part_exists:
+        print("  (Deixa en blanc si vols ignorar-ho)")
+        excel_fn_part = input("Nom del fitxer Excel (participants):") 
+        if excel_fn_part == '': break
+        excel_fn_part = (excel_fn_part if excel_fn_part.endswith('.xlsx') 
+                         else excel_fn_part+'.xlsx')
+        part_exists = os.path.isfile(excel_fn_part)
+        print(f"\n >>> {excel_fn_part} does NOT found. Try again," 
+              if excel_fn_part else "Found!")
+
+    return pd.read_excel(excel_fn_part, sheet_name=0) if part_exists else None
 
 def unify_columns(data, text_in_column, new_column_name, expected_items=1, 
                   expected_types=(float, int), delete=True):
-
-    grup_str = "Grup"
 
     keys = data.keys()
 
@@ -22,7 +80,7 @@ def unify_columns(data, text_in_column, new_column_name, expected_items=1,
 
     cols = data[keys[0]]
     new_values = np.zeros_like(cols)
-    grup_col = np.zeros_like(cols) if grup_str not in keys else None
+    grup_col = np.zeros_like(cols) if GRUP_LAB not in keys else None
 
     for idx, alumne in data.iterrows():
         vals = []
@@ -34,9 +92,9 @@ def unify_columns(data, text_in_column, new_column_name, expected_items=1,
             if type(value) in (float, int):
                 vals.append(value)
                 if grup_col is not None and not grup:
-                    match = regex.search("M1A|M1B|M2A|M2B|T1A|T1B", column)
+                    match = regex.search(POSSIBLE_GRUPS_LAB, column)
                     grup = match.group() if match else ""
-                matchS = regex.search(r"Sessió\s+(\d+)\s+", column)
+                matchS = regex.search(SESSION_PATTERN, column)
                 sess.append(int(matchS.group(1)) if matchS else "")
 
         if grup_col is not None:
@@ -53,13 +111,13 @@ def unify_columns(data, text_in_column, new_column_name, expected_items=1,
             else:
                 new_values[idx] = -1*abs(num_vals-expected_items)  # set an error code
                 if expected_items > 1 :
-                    missing = [str(s) for s in range(1, expected_items+1) if s not in sess]
+                    missing = ['s'+str(s) for s in range(1, expected_items+1) if s not in sess]
         
         miss_sess.append(','.join(missing))
 
 
     if grup_col is not None:
-        data[grup_str] = grup_col
+        data[GRUP_LAB] = grup_col
         
     data[new_column_name] = new_values
 
@@ -67,29 +125,39 @@ def unify_columns(data, text_in_column, new_column_name, expected_items=1,
         data = data.drop(columns=colums_names)
 
     if expected_items > 1:
-        data["Missing sessions"] = miss_sess
+        data[MISSING_SESSIONS] = miss_sess
 
     return data
 
 
-def unify_informes(data):
+def unify(data):
 
-    text_in_column = "entrega informes"
-    expected_items = 7
-    new_column_name = "Mitjana informes"
+    data = unify_columns(data, ORAL_PATTERN, ORAL_NEW, ORAL_EXPECTED)
 
-    return unify_columns(data, text_in_column, new_column_name, expected_items)
+    return unify_columns(data, INFORMES_PATTERN, INFORMES_NEW, INFORMES_EXPECTED)
 
 
-def unify_oral(data):
+def find_grup_teo(data, data_part):
 
-    text_in_column = "prova oral"
-    expected_items = 1
-    new_column_name = "Prova oral unif."
+    if data_part is None:
+        return data
 
-    return unify_columns(data, text_in_column, new_column_name, expected_items)
+    grup_teo_list = []
+    for idx, alumne in data_part.iterrows():
+        grups_str = alumne[GRUPS_LABEL_CV].split(',')[0]
+        for cand in GRUPS_TEO:
+            if cand in grups_str:
+                gt = cand[3:] if cand.startswith('Q2') else 'ReAv.'
+                break
+            else:
+                gt = '-'
+        grup_teo_list.append(gt)
 
-def reformating(data):
+    data_part[GRUP_TEO] = grup_teo_list
+    return data.merge(data_part[[NIUB, GRUP_TEO]], on=NIUB, how='left')
+
+
+def reformating(data, keep_AC=False):
     """ Long names are not usefull
     """
     keys = data.keys()
@@ -102,18 +170,70 @@ def reformating(data):
                      for c in renaming_olds]
 
     renaming = {k: v for k, v in zip(renaming_olds, renaming_news)}
-
+    
     data = data.rename(columns=renaming)
 
-    data = data.drop(columns=["Darrera descàrrega des d'aquest curs",
-                              "Adreça electrònica"])
+    desired_columns = FINAL_KEYS + renaming_news if keep_AC else FINAL_KEYS
 
+    drop_keys = [k for k in data.keys() if k not in desired_columns]
+    data = data.drop(columns=drop_keys)
 
     return data
 
+def print_summary(data):
+
+    print("Resulting file:\n")
+    print(data[:5])
+    print("  (això és un resum de la taula final)")
+    print("---------------\n")
+
+    print("No values found: (repetidors?)")
+    repes = data[(data[INFORMES_NEW]==-9) & (data[ORAL_NEW]==-9)]
+    print('\n'.join([rn+' '+rc for rn, rc in zip(repes['Nom'], repes['Cognoms'])])
+          if len(repes)<10 else f"{len(repes)} estudiants no tenen cap nota. ")
+    print("---------------\n")
+
+    print("Missing grades: (falta alguna nota?)")
+    missi = data[((data[INFORMES_NEW]>-9) &
+                  (data[INFORMES_NEW]<0) )    | 
+                 ((data[ORAL_NEW]>-9) &
+                  (data[ORAL_NEW]<0))]
+
+    missing_dict = {}
+    for idx, m_st in missi.iterrows():
+        grup = m_st[GRUP_LAB]
+        num1, num2 = missing_dict.get(grup, (0, 0))
+        missing_dict[grup] = (num1+1, num2+m_st[INFORMES_NEW])
+
+    for gr in missing_dict.keys():
+        print(f"{gr}: {-missing_dict[gr][1]} (de {missing_dict[gr][0]} alumnes)")
+    print(" ")    
+
+    print('Sessions que falten:')
+    print('\n'.join([f'{nota}: ({grup}) {rn} {rc} -> {sess}' 
+                     for rn, rc, nota, grup, sess 
+                     in zip(missi['Nom'], missi['Cognoms'], 
+                            missi[INFORMES_NEW], missi[GRUP_LAB], 
+                            missi[MISSING_SESSIONS])]))
+    print("---------------\n")
+
+def export_to_excel(data, filename, new_filename=None):
+    new_filename = new_filename or filename.replace('.xlsx', UNIFIED_SUFFIX+".xlsx")
+    try:
+        if GRUP_TEO in data.keys():
+            with pd.ExcelWriter(new_filename, engine='openpyxl') as writer:
+                for nom_grup, df_grup in data.groupby(GRUP_TEO):
+                    if nom_grup != Q1T1_LABEL:
+                        df_grup.to_excel(writer, sheet_name=nom_grup, index=False)
+        else:
+            data.to_excel(new_filename)
+    except PermissionError:
+        print(" >>> No s'ha pogut desar, probablement perquè el document ja existeix "
+              "i ESTÀ OBERT.\n     Pot existir, però ha d'estar tancat.")
+    
 
 def export(data, filename):
-    data.to_excel(filename.replace('.xlsx', "_unified.xlsx"))
+    data.to_excel(filename.replace('.xlsx', UNIFIED_SUFFIX+".xlsx"))
 
 
 if __name__ == "__main__":
